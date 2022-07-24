f18f714dbfc783b063dd35589892f1ff1535dd00
Luke <lukeedwards30167116@gmail.com>
2022-07-25 00:23:35 +0100

    chore: fix navbar order

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
-import type { RouteRecordNormalized } from 'vue-router'
+interface Route {
+  name: string
+  path: string
+}
 
-const router = useRouter()
 const currentRoute = useRoute()
 
-const routes = computed(() => router.getRoutes().filter(route => route.meta.navbar))
-const isCurrentRoute = (route: RouteRecordNormalized) => route.name === currentRoute.name
+const routes: Array<Route> = [
+  {
+    path: '/games',
+    name: 'games',
+  }, {
+    path: '/about',
+    name: 'about',
+  }, {
+    path: '/contact',
+    name: 'contact',
+  },
+]
+const isCurrentRoute = (route: Route) => route.name === currentRoute.name
 </script>
 
 <template>
@@ -13,8 +26,8 @@ const isCurrentRoute = (route: RouteRecordNormalized) => route.name === currentR
     <router-link to="/" font-serif text-8 style="letter-spacing: 0.1em;">
       Alter Eco
     </router-link>
-    <ul flex ml-auto>
-      <li v-for="route in routes" :key="route.path" px-8 text-accent-400 :class="{ 'text-brand-secondary': isCurrentRoute(route) }">
+    <ul flex ml-auto text-accent-400 text-5 font-serif style="letter-spacing: 0.1em;">
+      <li v-for="route in routes" :key="route.path" px-8 :class="{ 'text-brand-secondary': isCurrentRoute(route) }">
         <router-link :to="route.path">
           {{ route.name }}
         </router-link>
