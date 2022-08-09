88021b7cf003ef833556184216a6c1cafae59b60
Luke Edwards <lukeedwards30167116@gmail.com>
2022-08-09 14:05:52 +0100

    chore: unocss rule

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,5 +1,6 @@
 import {
   defineConfig,
+  toEscapedSelector as e,
   presetAttributify,
   presetIcons,
   presetTypography,
@@ -10,22 +11,52 @@ import {
 } from 'unocss'
 
 export default defineConfig({
+  rules: [
+    [/^lsp-(.+)$/, ([, name], { rawSelector }) => {
+      // remove tag end if there
+      if (name.includes('>'))
+        name = name.substring(0, name.length - 1)
+
+      // discard mismatched rules
+      if (/[a-zA-Z]+$/.test(name))
+        return
+
+      const selector = e(rawSelector)
+      // return a string instead of an object
+      return `
+      ${selector} {
+        letter-spacing: 0.${name}em;
+      }`
+    }],
+  ],
+  theme: {
+    colors: {
+      nav: {
+        bg: '#D9D9D9',
+        text: '#818792',
+      },
+      brand: {
+        primary: '#31A5A7',
+        secondary: '#78B2C4',
+      },
+    },
+  },
   presets: [
     presetUno(),
     presetAttributify(),
     presetIcons({
-      scale: 1.2,
+      scale: 2,
       warn: true,
     }),
     presetTypography(),
     presetWebFonts({
+      provider: 'google',
       fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono',
+        sans: 'Noto Sans',
+        serif: 'Oswald',
       },
     }),
   ],
   transformers: [transformerDirectives(), transformerVariantGroup()],
-  safelist: 'prose prose-sm m-auto text-left'.split(' '),
+  safelist: 'prose prose-sm m-auto text-left i-carbon-game-console i-carbon-home i-carbon-debug'.split(' '),
 })
