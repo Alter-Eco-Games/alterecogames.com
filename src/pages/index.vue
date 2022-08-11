f37da240336d0bb3aaab13562a2e18e7dc574692
Luke Edwards <lukeedwards30167116@gmail.com>
2022-08-11 15:47:08 +0100

    fix: index page responsiveness

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -17,9 +17,9 @@ const isMobile = breakpoints.smaller('sm')
 
 <template>
   <div flex justify-center items-start mt-40 px-8 lt-sm:flex-col lt-sm:justify-start lt-sm:mt-0 lt-sm:items-center>
-    <div mr-32 lt-sm:mr-0>
+    <div mr-32 lt-sm:mr-0 lt-md:mr-16 lt-lg:mr-20>
       <div mb-24 lt-sm:mb-16>
-        <h1 font-serif lsp-1 text-16 lt-sm:text-8 truncate text-center>
+        <h1 font-serif lsp-1 text-16 truncate lt-sm:text-8 lt-lg:text-10 lt-md:text-7>
           Control <span text-brand-primary>your</span> ads
         </h1>
         <p text-400 lt-sm:text-3>
@@ -28,7 +28,7 @@ const isMobile = breakpoints.smaller('sm')
       </div>
       <div v-if="!isMobile">
         <router-link to="/games">
-          <button btn-primary mr-8>
+          <button btn-primary mr-8 lt-md:mr-2 lt-lg:mb-4>
             games
           </button>
         </router-link>
@@ -39,7 +39,7 @@ const isMobile = breakpoints.smaller('sm')
         </router-link>
       </div>
     </div>
-    <img src="../assets/helping.svg" w-lg lt-sm:mb-30>
+    <img src="../assets/helping.svg" w-a lt-xl:w-sm lt-lg:w-xs lt-sm:mb-30>
     <router-link v-if="isMobile" to="/charities">
       <button btn-secondary px-28 py-4>
         charities
